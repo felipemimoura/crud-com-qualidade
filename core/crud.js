50f8a017fdd3df570f8fce1c5975e2b0f4861a8f
Felipe Moura <felipemimoura@gmail.com>
2024-01-06 11:45:50 -0300

    Aula 2.1

---
+++ core/crud.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const DB_FILE_PATH = "./core/db"
+console.log(" [CRUD]");
+
+function create(content) {
+  //Salvar o content no sistema
+  fs.writeFileSync(DB_FILE_PATH, content)
+  return content;
+}
+
+// [Simulation]
+create("Hoje eu preciso gravar aulas")
+
+
+
